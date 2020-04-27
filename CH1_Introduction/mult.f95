f0acde1b21b262851e40888f6d8deaa9b3454b72
Paul McQuade <paulmcquad@gmail.com>
2020-04-27 17:36:38 +0100

    Added more Fortran Programs
    mult, roots and power
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ CH1_Introduction/mult.f95
@@ -0,0 +1,20 @@
+program mult
+    implicit none
+    !Purpose:
+    !To illustrate some of the basic features of a Fortran program.
+    !
+    ! Declare the variables used in this program.
+    INTEGER :: i, j, k ! All variables are integers
+    
+    ! Get two values to store in variables i and j
+    WRITE (*,*) 'Enter the numbers to multiply: '
+    READ (*,*) i, j
+
+    ! Multiply the numbers together
+    k = i * j
+    ! Write out the result.
+    WRITE (*,*) 'Result = ', k
+    ! Finish up.
+    STOP
+
+end program mult
